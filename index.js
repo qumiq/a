e8639f4e316c3cbf33c379beb2be5e11250985a0
qumiq <164170554+qumiq@users.noreply.github.com>
2024-11-08 22:10:53 +0800

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,10 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["boost swikomu! ദ്ദി ˉ͈̀꒳ˉ͈́ )✧", "boost swikomu! ദ്ദി ˉ͈̀꒳ˉ͈́ )✧"];
+client.user.setActivity({
+        name: 'boost swikomu! ദ്ദി ˉ͈̀꒳ˉ͈́ )✧',
+        type: ActivityType.Playing,
+    })
 let currentIndex = 0;
 
 async function login() {
@@ -54,10 +57,6 @@ async function login() {
 
 function updateStatus() {
   const currentStatus = statusMessages[currentIndex];
-  client.user.setPresence({
-    activities: [{ name: currentStatus, type: ActivityType.Listening }],
-    status: 'online',
-  });
   console.log('\x1b[33m[ STATUS ]\x1b[0m', `Updated status to: ${currentStatus}`);
   currentIndex = (currentIndex + 1) % statusMessages.length;
 }
